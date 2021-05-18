2e2a199715e6465328ae8de9af6d3c65caa33e18
hanabpg3 <84052764+hanabpg3@users.noreply.github.com>
2021-05-18 18:05:37 +0900

    title,favicon modify

--- src/hooks/useGetDocumentTitlePrice.ts
+++ src/hooks/useGetDocumentTitlePrice.ts
@@ -16,7 +16,7 @@ const useGetDocumentTitlePrice = () => {
         })}`
 
   useEffect(() => {
-    document.title = `PancakeSwap${cakePriceUsdString}`
+    document.title = `TaalSwap${cakePriceUsdString}`
   }, [cakePriceUsdString])
 }
 export default useGetDocumentTitlePrice
