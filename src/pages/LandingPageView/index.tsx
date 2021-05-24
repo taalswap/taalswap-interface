7d9ce06ea9885f17b5eafb9af34d53370286c85a
helloljho <helloljho@gmail.com>
2021-05-24 16:04:47 -0700

    add landing page

--- src/pages/LandingPageView/index.tsx
+++ src/pages/LandingPageView/index.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+import TopBar from './TopBar';
+
+const AppWrapper = styled.div`
+  height: '100%';
+  width: '100%';
+  border: '1px solid blue';
+`;
 
 const LandingPageView = () => {
-  return <div>LandingPageView</div>;
+  return (
+    <div style={{ border: '1px solid blue', width: '100%', height: '100%' }}>
+      <div>
+        <TopBar />
+      </div>
+      <div>Landing Page file 위치 : /src/pages/LandingPageView/index.tsx</div>
+      <br />
+      <br />
+      <div>LandingPageView</div>
+      <div>LandingPageView</div>
+      <div>LandingPageView</div>
+      <div>LandingPageView</div>
+    </div>
+  );
 };
 
 export default LandingPageView;
