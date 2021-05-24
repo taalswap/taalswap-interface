1d88dcc1628049d344b62ef368d54b50d707ea33
Peter Nahm <hwnahm@gmail.com>
2021-05-24 19:52:55 +0900

    Solve key duplicate

--- src/constants/localisation/languageCodes.ts
+++ src/constants/localisation/languageCodes.ts
@@ -1,31 +1,31 @@
-export const EN = { code: 'en', language: 'English', locale: 'en' }
-export const AR = { code: 'ar', language: 'العربية', locale: 'ar' }
-export const BN = { code: 'bn', language: 'বাংলা', locale: 'bn' }
-export const ZHCN = { code: 'zh-CN', language: '简体中文', locale: 'zh-CN' }
-export const ZHTW = { code: 'zh-TW', language: '繁體中文', locale: 'zh-TW' }
-export const NL = { code: 'nl', language: 'Nederlands', locale: 'nl' }
-export const FIL = { code: 'fil', language: 'Filipino', locale: 'fil' }
-export const FI = { code: 'fi', language: 'Suomalainen', locale: 'fi' }
-export const FR = { code: 'fr', language: 'Français', locale: 'fr' }
-export const DE = { code: 'de', language: 'Deutsch', locale: 'de' }
-export const EL = { code: 'el', language: 'Ελληνικά', locale: 'el' }
-export const HI = { code: 'hi', language: 'हिंदी', locale: 'hi' }
-export const HU = { code: 'hu', language: 'Magyar', locale: 'hu' }
-export const ID = { code: 'id', language: 'Bahasa Indonesia', locale: 'id' }
-export const IT = { code: 'it', language: 'Italiano', locale: 'it' }
-export const JA = { code: 'ja', language: '日本語', locale: 'ja' }
-export const KO = { code: 'ko', language: '한국어', locale: 'ko' }
-export const PL = { code: 'pl', language: 'Polski', locale: 'en' }
-export const PTPT = { code: 'pt-PT', language: 'Português (Portugal)', locale: 'pt-PT' }
-export const PTBR = { code: 'pt-BR', language: 'Português (Brasil)', locale: 'pt-BR' }
-export const RO = { code: 'ro', language: 'Română', locale: 'ro' }
-export const RU = { code: 'ru', language: 'Русский', locale: 'ru' }
-export const ESES = { code: 'es-ES', language: 'Español', locale: 'es-ES' }
-export const SVSE = { code: 'sv-SE', language: 'Svenska', locale: 'sv-SE' }
-export const TA = { code: 'ta', language: 'தமிழ்', locale: 'ta' }
-export const TR = { code: 'tr', language: 'Türkçe', locale: 'tr' }
-export const UK = { code: 'uk', language: 'Українська', locale: 'uk' }
-export const VI = { code: 'vi', language: 'Tiếng Việt', locale: 'vi' }
+export const EN = { code: 'en', language: 'English', locale: 'en' };
+export const AR = { code: 'ar', language: 'العربية', locale: 'ar' };
+export const BN = { code: 'bn', language: 'বাংলা', locale: 'bn' };
+export const ZHCN = { code: 'zh-CN', language: '简体中文', locale: 'zh-CN' };
+export const ZHTW = { code: 'zh-TW', language: '繁體中文', locale: 'zh-TW' };
+export const NL = { code: 'nl', language: 'Nederlands', locale: 'nl' };
+export const FIL = { code: 'fil', language: 'Filipino', locale: 'fil' };
+export const FI = { code: 'fi', language: 'Suomalainen', locale: 'fi' };
+export const FR = { code: 'fr', language: 'Français', locale: 'fr' };
+export const DE = { code: 'de', language: 'Deutsch', locale: 'de' };
+export const EL = { code: 'el', language: 'Ελληνικά', locale: 'el' };
+export const HI = { code: 'hi', language: 'हिंदी', locale: 'hi' };
+export const HU = { code: 'hu', language: 'Magyar', locale: 'hu' };
+export const ID = { code: 'id', language: 'Bahasa Indonesia', locale: 'id' };
+export const IT = { code: 'it', language: 'Italiano', locale: 'it' };
+export const JA = { code: 'ja', language: '日本語', locale: 'ja' };
+export const KO = { code: 'ko', language: '한국어', locale: 'ko' };
+export const PL = { code: 'pl', language: 'Polski', locale: 'pl' };
+export const PTPT = { code: 'pt-PT', language: 'Português (Portugal)', locale: 'pt-PT' };
+export const PTBR = { code: 'pt-BR', language: 'Português (Brasil)', locale: 'pt-BR' };
+export const RO = { code: 'ro', language: 'Română', locale: 'ro' };
+export const RU = { code: 'ru', language: 'Русский', locale: 'ru' };
+export const ESES = { code: 'es-ES', language: 'Español', locale: 'es-ES' };
+export const SVSE = { code: 'sv-SE', language: 'Svenska', locale: 'sv-SE' };
+export const TA = { code: 'ta', language: 'தமிழ்', locale: 'ta' };
+export const TR = { code: 'tr', language: 'Türkçe', locale: 'tr' };
+export const UK = { code: 'uk', language: 'Українська', locale: 'uk' };
+export const VI = { code: 'vi', language: 'Tiếng Việt', locale: 'vi' };
 
 export const allLanguages = [
   EN,
@@ -56,4 +56,4 @@ export const allLanguages = [
   TR,
   UK,
   VI
-]
+];
