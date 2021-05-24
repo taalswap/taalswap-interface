e9f1b8491adde49a1dbd040cc824e82d66fbc900
Peter Nahm <hwnahm@gmail.com>
2021-05-24 19:53:16 +0900

    Migrate to Ethereum #3

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,32 +7,32 @@ type ChainTokenList = {
   readonly [chainId in ChainId]: Token[]
 }
 
-export const CAKE = new Token(
-  ChainId.MAINNET,
-  '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
+export const TAL = new Token(
+  ChainId.RINKEBY,
+  '0x94dD914019BB5E821dC1edD1e5d8bC618A251a80',
   18,
-  'CAKE',
-  'PancakeSwap Token'
+  'TAL',
+  'TaalSwap Token'
 );
-export const WBNB = new Token(ChainId.MAINNET, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB');
-export const DAI = new Token(ChainId.MAINNET, '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', 18, 'DAI', 'Dai Stablecoin');
-export const BUSD = new Token(ChainId.MAINNET, '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', 18, 'BUSD', 'Binance USD');
-export const BTCB = new Token(ChainId.MAINNET, '0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c', 18, 'BTCB', 'Binance BTC');
-export const USDT = new Token(ChainId.MAINNET, '0x55d398326f99059fF775485246999027B3197955', 18, 'USDT', 'Tether USD');
-export const UST = new Token(
-  ChainId.MAINNET,
-  '0x23396cF899Ca06c4472205fC903bDB4de249D6fC',
-  18,
-  'UST',
-  'Wrapped UST Token'
-);
-export const ETH = new Token(
-  ChainId.MAINNET,
-  '0x2170Ed0880ac9A755fd29B2688956BD959F933F8',
-  18,
-  'ETH',
-  'Binance-Peg Ethereum Token'
-);
+// export const WETH = new Token(ChainId.RINKEBY, '0x92EcACFC94588aa99fba837Be1a98738290E3252', 18, 'WETH', 'Wrapped ETH');
+// export const DAI = new Token(ChainId.MAINNET, '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', 18, 'DAI', 'Dai Stablecoin');
+export const BUSD = new Token(ChainId.RINKEBY, '0xd16431da4EafE953B4f34923CdB8d833FB1B2E7c', 18, 'BUSD', 'Binance USD');
+// export const BTCB = new Token(ChainId.MAINNET, '0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c', 18, 'BTCB', 'Binance BTC');
+export const USDT = new Token(ChainId.RINKEBY, '0xC958c2ACE36870471238319Bc29018cC549C126D', 18, 'USDT', 'Tether USD');
+// export const UST = new Token(
+//   ChainId.MAINNET,
+//   '0x23396cF899Ca06c4472205fC903bDB4de249D6fC',
+//   18,
+//   'UST',
+//   'Wrapped UST Token'
+// );
+// export const ETH = new Token(
+//   ChainId.MAINNET,
+//   '0x2170Ed0880ac9A755fd29B2688956BD959F933F8',
+//   18,
+//   'ETH',
+//   'Binance-Peg Ethereum Token'
+// );
 
 const WETH_ONLY: ChainTokenList = {
   [ChainId.MAINNET]: [WETH[ChainId.MAINNET]],
@@ -43,7 +43,8 @@ const WETH_ONLY: ChainTokenList = {
 // used to construct intermediary pairs for trading
 export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
   ...WETH_ONLY,
-  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DAI, BUSD, BTCB, USDT, UST, ETH]
+  [ChainId.RINKEBY]: [...WETH_ONLY[ChainId.RINKEBY], BUSD, USDT]
+  // [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DAI, BUSD, BTCB, USDT, UST, ETH]
 };
 
 /**
@@ -51,26 +52,30 @@ export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
  * tokens.
  */
 export const CUSTOM_BASES: { [chainId in ChainId]?: { [tokenAddress: string]: Token[] } } = {
-  [ChainId.MAINNET]: {}
+  [ChainId.RINKEBY]: {}
 };
 
 // used for display in the default list when adding liquidity
 export const SUGGESTED_BASES: ChainTokenList = {
   ...WETH_ONLY,
-  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DAI, BUSD, USDT]
+  [ChainId.RINKEBY]: [...WETH_ONLY[ChainId.RINKEBY], BUSD, USDT]
+  // [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DAI, BUSD, USDT]
 };
 
 // used to construct the list of all pairs we consider by default in the frontend
 export const BASES_TO_TRACK_LIQUIDITY_FOR: ChainTokenList = {
   ...WETH_ONLY,
-  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DAI, BUSD, BTCB, USDT]
+  [ChainId.RINKEBY]: [...WETH_ONLY[ChainId.RINKEBY], BUSD, USDT]
+  // [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DAI, BUSD, BTCB, USDT]
 };
 
 export const PINNED_PAIRS: { readonly [chainId in ChainId]?: [Token, Token][] } = {
-  [ChainId.MAINNET]: [
-    [CAKE, WBNB],
-    [BUSD, USDT],
-    [DAI, USDT]
+  // [ChainId.MAINNET]: [
+  [ChainId.RINKEBY]: [
+    [TAL, WETH[ChainId.RINKEBY]],
+    // [CAKE, WBNB],
+    [BUSD, USDT]
+    // [DAI, USDT]
   ]
 };
 
--- src/hooks/useGetDocumentTitlePrice.ts
+++ src/hooks/useGetDocumentTitlePrice.ts
@@ -1,22 +1,22 @@
-import { useEffect } from 'react'
-import useGetPriceData from './useGetPriceData'
-import { CAKE } from '../constants'
+import { useEffect } from 'react';
+import useGetPriceData from './useGetPriceData';
+import { TAL } from '../constants';
 
 const useGetDocumentTitlePrice = () => {
-  const priceData = useGetPriceData()
+  const priceData = useGetPriceData();
 
-  const cakePriceUsd = priceData ? parseFloat(priceData.data[CAKE.address].price) : 0
+  const cakePriceUsd = priceData ? parseFloat(priceData.data[TAL.address].price) : 0;
 
   const cakePriceUsdString =
     Number.isNaN(cakePriceUsd) || cakePriceUsd === 0
       ? ''
       : ` - $${cakePriceUsd.toLocaleString(undefined, {
-          minimumFractionDigits: 3,
-          maximumFractionDigits: 3,
-        })}`
+        minimumFractionDigits: 3,
+        maximumFractionDigits: 3
+      })}`;
 
   useEffect(() => {
-    document.title = `TaalSwap${cakePriceUsdString}`
-  }, [cakePriceUsdString])
-}
-export default useGetDocumentTitlePrice
+    document.title = `TaalSwap${cakePriceUsdString}`;
+  }, [cakePriceUsdString]);
+};
+export default useGetDocumentTitlePrice;
