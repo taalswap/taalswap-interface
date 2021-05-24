b253147c9c3b9d7e8e599beec6f6db86a120eaa6
Peter Nahm <hwnahm@gmail.com>
2021-05-24 17:19:14 +0900

    Migrate to Ethereum

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -1,9 +1,10 @@
-import { ChainId } from 'taalswap-sdk'
-import MULTICALL_ABI from './abi.json'
+import { ChainId } from 'taalswap-sdk';
+import MULTICALL_ABI from './abi.json';
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb', // TODO
-  [ChainId.BSCTESTNET]: '0x301907b5835a2d723Fe3e9E8C5Bc5375d5c1236A'
-}
+  [ChainId.ROPSTEN]: '0x301907b5835a2d723Fe3e9E8C5Bc5375d5c1236A',
+  [ChainId.RINKEBY]: '0x9B1265E5a12DED869CBe1934828aCe8388FA1362'
+};
 
-export { MULTICALL_ABI, MULTICALL_NETWORKS }
+export { MULTICALL_ABI, MULTICALL_NETWORKS };
