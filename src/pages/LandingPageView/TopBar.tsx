96b0315151e08c05cb776397bbeb626b6ca3fffb
helloljho <helloljho@gmail.com>
2021-06-02 12:04:31 +0900

    change link url

--- src/pages/LandingPageView/TopBar.tsx
+++ src/pages/LandingPageView/TopBar.tsx
@@ -1,36 +1,60 @@
 import React from 'react';
+import { useWeb3React } from '@web3-react/core';
 import { Target } from 'react-feather';
 import { Link } from 'taalswap-uikit';
-import Topmenu from './topmenu'
+import Topmenu from './topmenu';
 import logo_img from './images/TAAL_Logo.png';
 
 const TopBar = () => {
+  const { account } = useWeb3React();
+
+  console.log(account);
   return (
-    <div className='tabbar_wrap'>
+    <div className="tabbar_wrap">
       <div>
-        <Link href="/">
-          <img src={logo_img} alt="logo_img" className='top_logo'/>
+        <Link href="http://localhost:3001/">
+          <img src={logo_img} alt="logo_img" className="top_logo" />
         </Link>
       </div>
-      <div className='top_menu'>
+      <div className="top_menu">
         <div>
-          <Link href="/" style={{color: '#212b36', textDecoration: 'none'}}>Home</Link>
+          <Link
+            href="http://localhost:3001/"
+            style={{ color: '#212b36', textDecoration: 'none' }}
+          >
+            Home
+          </Link>
         </div>
         <div style={{ marginLeft: '30px' }}>
-          <Link href="#/swap" style={{color: '#212b36', textDecoration: 'none'}}>Swap</Link>
+          <Link
+            href="#/swap"
+            style={{ color: '#212b36', textDecoration: 'none' }}
+          >
+            Swap
+          </Link>
         </div>
         <div style={{ marginLeft: '30px' }}>
-          <Link href="#/pool" style={{color: '#212b36', textDecoration: 'none'}}>Liquidity</Link>
+          <Link
+            href="#/pool"
+            style={{ color: '#212b36', textDecoration: 'none' }}
+          >
+            Liquidity
+          </Link>
         </div>
         <div style={{ marginLeft: '30px' }}>
-          <Link href="http://localhost:3001/farms" style={{color: '#212b36', textDecoration: 'none'}}>Farms</Link>
+          <Link
+            href="http://localhost:3001/farms"
+            style={{ color: '#212b36', textDecoration: 'none' }}
+          >
+            Farms
+          </Link>
         </div>
         <div>
-          <input type="button" value="Connect Wallet" className='connect_btn'/>
+          <input type="button" value="Connect Wallet" className="connect_btn" />
         </div>
       </div>
-      <div className='mobile_menu' style={{cursor:'pointer'}}>
-          <Topmenu />
+      <div className="mobile_menu" style={{ cursor: 'pointer' }}>
+        <Topmenu />
       </div>
     </div>
   );
