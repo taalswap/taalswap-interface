ff910f5de75fe770b7b87ebb652b3210f9148bd6
Peter Nahm <hwnahm@gmail.com>
2021-06-22 19:19:20 +0900

    Apply new tooltip on HelpIcon

--- src/components/NavigationTabs/index.tsx
+++ src/components/NavigationTabs/index.tsx
@@ -5,6 +5,11 @@ import { ArrowLeft } from 'react-feather'
 import { RowBetween } from 'components/Row'
 import QuestionHelper from 'components/QuestionHelper'
 import useI18n from 'hooks/useI18n'
+import { HelpIcon, useTooltip } from 'taalswap-uikit';
+
+const ReferenceElement = styled.div`
+  display: flex;
+`
 
 const Tabs = styled.div`
   display: flex;
@@ -23,6 +28,24 @@ const StyledArrowLeft = styled(ArrowLeft)`
   color: ${({ theme }) => theme.colors.text};
 `
 
+const TipFind = () => {
+  const TranslateString = useI18n()
+
+  const { targetRef, tooltip, tooltipVisible } = useTooltip(
+    TranslateString(256, 'Use this tool to find pairs that do not automatically appear in the interface.'),
+    { placement: 'top-end', tooltipOffset: [20, 10] },
+  )
+
+  return (
+    <div>
+      <ReferenceElement ref={targetRef}>
+        <HelpIcon color="textSubtle" />
+      </ReferenceElement>
+      {tooltipVisible && tooltip}
+    </div>
+  )
+}
+
 export function FindPoolTabs() {
   const TranslateString = useI18n()
   return (
@@ -32,11 +55,37 @@ export function FindPoolTabs() {
           <StyledArrowLeft />
         </HistoryLink>
         <ActiveText>Import Pool</ActiveText>
-        <QuestionHelper
-          text={TranslateString(256, 'Use this tool to find pairs that do not automatically appear in the interface.')}
-        />
+        <TipFind />
       </RowBetween>
     </Tabs>
+  )
+}
+
+const TipAdding = ({ adding }: { adding: boolean }) => {
+  const TranslateString = useI18n()
+
+  const text = adding ?
+    TranslateString(
+      264,
+      'When you add liquidity, you are given pool tokens representing your position. These tokens automatically earn fees proportional to your share of the pool, and can be redeemed at any time.'
+    )
+    : TranslateString(
+      266,
+      'Removing pool tokens converts your position back into underlying tokens at the current rate, proportional to your share of the pool. Accrued fees are included in the amounts you receive.'
+    )
+
+  const { targetRef, tooltip, tooltipVisible } = useTooltip(
+    text,
+    { placement: 'top-end', tooltipOffset: [20, 10] },
+  )
+
+  return (
+    <div>
+      <ReferenceElement ref={targetRef}>
+        <HelpIcon color="textSubtle" />
+      </ReferenceElement>
+      {tooltipVisible && tooltip}
+    </div>
   )
 }
 
@@ -49,19 +98,7 @@ export function AddRemoveTabs({ adding }: { adding: boolean }) {
           <StyledArrowLeft />
         </HistoryLink>
         <ActiveText>{adding ? TranslateString(258, 'Add') : TranslateString(260, 'Remove')} Liquidity</ActiveText>
-        <QuestionHelper
-          text={
-            adding
-              ? TranslateString(
-                  264,
-                  'When you add liquidity, you are given pool tokens representing your position. These tokens automatically earn fees proportional to your share of the pool, and can be redeemed at any time.'
-                )
-              : TranslateString(
-                  266,
-                  'Removing pool tokens converts your position back into underlying tokens at the current rate, proportional to your share of the pool. Accrued fees are included in the amounts you receive.'
-                )
-          }
-        />
+        <TipAdding adding={adding} />
       </RowBetween>
     </Tabs>
   )
