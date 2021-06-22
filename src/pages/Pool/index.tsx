c86580cbf2c86d6df42de2fadbd82cd4bf94dad8
Peter Nahm <hwnahm@gmail.com>
2021-06-22 20:03:20 +0900

    Change currency symbol

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -74,7 +74,7 @@ export default function Pool() {
           title={TranslateString(262, 'Liquidity')}
           description={TranslateString(1168, 'Add liquidity to receive LP tokens')}
         >
-          <Button id='join-pool-button' as={Link} to='/add/BNB' mb='16px'>
+          <Button id='join-pool-button' as={Link} to='/add/ETH' mb='16px'>
             {TranslateString(168, 'Add Liquidity')}
           </Button>
         </PageHeader>
