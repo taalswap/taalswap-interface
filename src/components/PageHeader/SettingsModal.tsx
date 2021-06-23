da4d6dd4eb532d6058cc94c34f7ae704ba23662f
hanabpg3 <84052764+hanabpg3@users.noreply.github.com>
2021-06-23 18:40:56 +0900

    design modify

--- src/components/PageHeader/SettingsModal.tsx
+++ src/components/PageHeader/SettingsModal.tsx
@@ -21,7 +21,7 @@ const SettingsModal = ({ onDismiss = defaultOnDismiss, translateString }: Settin
   return (
     <Modal title={translateString(1200, 'Settings')} onDismiss={onDismiss} style={{position:'relative'}}>
       <div style={{position:"absolute",right:"20px",top:'20px',cursor:'pointer'}}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="33.941" height="33.941" viewBox="0 0 33.941 33.941">
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" onClick={onDismiss}>
   <g id="___Icons_ic_replace" data-name="__🥬Icons/ ic_replace" transform="translate(0 16.971) rotate(-45)">
     <g id="_gr" data-name="#gr">
       <path id="Path" d="M22.5,10.5h-9v-9a1.5,1.5,0,0,0-3,0v9h-9a1.5,1.5,0,0,0,0,3h9v9a1.5,1.5,0,0,0,3,0v-9h9a1.5,1.5,0,0,0,0-3Z" fill={btnColor}/>
