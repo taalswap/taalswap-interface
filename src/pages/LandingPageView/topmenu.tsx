59646cc0f915401b1100686958f029f72c9cd559
Peter Nahm <hwnahm@gmail.com>
2021-06-28 19:24:34 +0900

    chore: change korean translation

--- src/pages/LandingPageView/topmenu.tsx
+++ src/pages/LandingPageView/topmenu.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Target } from 'react-feather';
 import { Link } from 'taalswap-uikit';
+import { useTranslation } from '../../contexts/Localization';
 
 const Search = () => {
   const [showResults, setShowResults] = React.useState(false);
@@ -13,40 +14,43 @@ const Search = () => {
   );
 };
 
-const Results = () => (
-  <div className="hide_menu" id="results">
-    <span className="arrow_box">-</span>
-    <ul>
-      <li>
-        <Link href="/" style={{ color: '#00ab55', textDecoration: 'none' }}>
-          <span className="home_icon">home_icon</span>Home
-        </Link>
-      </li>
-      <li>
-        <Link href="#/swap" style={{ textDecoration: 'none' }}>
-          <span className="swap_icon">swap_icon</span>Swap
-        </Link>
-      </li>
-      <li>
-        <Link href="#/liquidity" style={{ textDecoration: 'none' }}>
-          <span className="liquidity_icon">liquidity_icon</span>Liquidity
-        </Link>
-      </li>
-      <li>
-        <Link
-          href="http://localhost:3001/farms"
-          style={{ textDecoration: 'none' }}
-        >
-          <span className="farms_icon">Farms_icon</span>Farms
-        </Link>
-      </li>
-      <li>
-        <Link href="/" style={{ textDecoration: 'none' }}>
-          <span className="connect_icon">connect_icon</span>Connect Wallet
-        </Link>
-      </li>
-    </ul>
-  </div>
-);
+const Results = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="hide_menu" id="results">
+      <span className="arrow_box">-</span>
+      <ul>
+        <li>
+          <Link href="/" style={{ color: '#00ab55', textDecoration: 'none' }}>
+            <span className="home_icon">home_icon</span>Home
+          </Link>
+        </li>
+        <li>
+          <Link href="#/swap" style={{ textDecoration: 'none' }}>
+            <span className="swap_icon">swap_icon</span>Swap
+          </Link>
+        </li>
+        <li>
+          <Link href="#/liquidity" style={{ textDecoration: 'none' }}>
+            <span className="liquidity_icon">liquidity_icon</span>Liquidity
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="http://localhost:3001/farms"
+            style={{ textDecoration: 'none' }}
+          >
+            <span className="farms_icon">Farms_icon</span>Farms
+          </Link>
+        </li>
+        <li>
+          <Link href="/" style={{ textDecoration: 'none' }}>
+            <span className="connect_icon">connect_icon</span>{t('Connect Wallet')}
+          </Link>
+        </li>
+      </ul>
+    </div>
+  )
+};
 
 export default Search;
