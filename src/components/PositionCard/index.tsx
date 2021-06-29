4dfa42916074431df0fda23c961ff9cf52f73f16
Peter Nahm <hwnahm@gmail.com>
2021-06-29 10:40:12 +0900

    chore: add & change korean translation

--- src/components/PositionCard/index.tsx
+++ src/components/PositionCard/index.tsx
@@ -68,8 +68,8 @@ export function MinimalPositionCard({ pair, showUnwrapped = false }: PositionCar
             <AutoColumn gap="12px">
               <FixedHeightRow>
                 <RowFixed>
-                  <Text style={{ textTransform: 'uppercase', fontWeight: 600 }} fontSize="14px" color="textSubtle">
-                    LP Tokens in your Wallet
+                  <Text style={{ fontWeight: 600 }} fontSize="14px" color="textSubtle">
+                    {t('LP tokens in your Wallet')}
                   </Text>
                 </RowFixed>
               </FixedHeightRow>
