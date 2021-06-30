ead85463a359a7205aeca0b3096c21418caf3467
Peter Nahm <hwnahm@gmail.com>
2021-06-30 22:32:15 +0900

    Update for Ropsten Test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -74,7 +74,7 @@ Cypress.Commands.overwrite('visit', (original, url, options) => {
     onBeforeLoad(win) {
       options && options.onBeforeLoad && options.onBeforeLoad(win)
       win.localStorage.clear()
-      const provider = new JsonRpcProvider('https://rinkeby.infura.io/v3/4bf032f2d38a4ed6bb975b80d6340847', 4)
+      const provider = new JsonRpcProvider('https://ropsten.infura.io/v3/4bf032f2d38a4ed6bb975b80d6340847', 3)
       const signer = new Wallet(PRIVATE_KEY_TEST_NEVER_USE, provider)
       win.ethereum = new CustomizedBridge(signer, provider)
     }
