7aa8cb08b768fb83e437b045b6373472e0350215
helloljho <helloljho@gmail.com>
2021-06-30 14:14:02 +0900

    change page title

--- src/hooks/useGetDocumentTitlePrice.ts
+++ src/hooks/useGetDocumentTitlePrice.ts
@@ -1,19 +1,19 @@
 import { useEffect } from 'react';
-import useGetCakeBusdLpPrice from 'utils/useGetCakeBusdLpPrice'
+import useGetCakeBusdLpPrice from 'utils/useGetCakeBusdLpPrice';
 
 const useGetDocumentTitlePrice = () => {
-  const cakePriceBusd = useGetCakeBusdLpPrice()
+  const cakePriceBusd = useGetCakeBusdLpPrice();
 
   const cakePriceBusdString =
     Number.isNaN(cakePriceBusd) || cakePriceBusd === 0 || !cakePriceBusd
       ? ''
       : ` - $${cakePriceBusd.toLocaleString(undefined, {
-        minimumFractionDigits: 3,
-        maximumFractionDigits: 3
-      })}`;
+          minimumFractionDigits: 3,
+          maximumFractionDigits: 3,
+        })}`;
 
   useEffect(() => {
-      document.title = `PancakeSwap${cakePriceBusdString}`
-    }, [cakePriceBusdString])
+    document.title = `TaalSwap${cakePriceBusdString}`;
+  }, [cakePriceBusdString]);
 };
 export default useGetDocumentTitlePrice;
