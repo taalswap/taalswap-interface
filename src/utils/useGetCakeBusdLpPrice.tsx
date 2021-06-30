9d720415f51cfcd40cba97b882904dedf30efee9
Peter Nahm <hwnahm@gmail.com>
2021-06-30 22:32:15 +0900

    Update for Ropsten Test

--- src/utils/useGetCakeBusdLpPrice.tsx
+++ src/utils/useGetCakeBusdLpPrice.tsx
@@ -3,10 +3,10 @@ import { useTradeExactIn } from 'hooks/Trades'
 import { tryParseAmount } from 'state/swap/hooks'
 
 const useGetCakeBusdLpPrice = () => {
-  const talAddress = '0xe18E460d38441027b6672363d68C9088F3D773Bf'
-  const busdAddress = '0xd16431da4EafE953B4f34923CdB8d833FB1B2E7c'
+  const talAddress = '0x78A24ACCF5f557B004C1313b2cAd2E85c345d971'
+  const usdcAddress = '0x9c8FA1ee532f8Afe9F2E27f06FD836F3C9572f71'
   const inputCurrency = useCurrency(talAddress)
-  const outputCurrency = useCurrency(busdAddress)
+  const outputCurrency = useCurrency(usdcAddress)
   const parsedAmount = tryParseAmount('1', inputCurrency ?? undefined)
   const bestTradeExactIn = useTradeExactIn(parsedAmount, outputCurrency ?? undefined)
   const price = bestTradeExactIn?.executionPrice.toSignificant(6)
