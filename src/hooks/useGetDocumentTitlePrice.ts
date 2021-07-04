858610190ed47f2b57c22991aad253787162a5e3
Peter Nahm <hwnahm@gmail.com>
2021-07-05 02:22:54 +0900

    fix: tal price on meta title

--- src/hooks/useGetDocumentTitlePrice.ts
+++ src/hooks/useGetDocumentTitlePrice.ts
@@ -1,8 +1,22 @@
-import { useEffect } from 'react';
-import useGetCakeBusdLpPrice from 'utils/useGetCakeBusdLpPrice';
+import { useEffect, useRef, useState } from 'react';
+// import useGetCakeBusdLpPrice from 'utils/useGetCakeBusdLpPrice';
+import useGetPriceData from './useGetPriceData';
 
 const useGetDocumentTitlePrice = () => {
-  const cakePriceBusd = useGetCakeBusdLpPrice();
+  // const cakePriceBusd = useGetCakeBusdLpPrice();
+  let cakePriceBusd = parseFloat('0.0');
+  const priceData = useGetPriceData();
+
+  if (priceData) {
+    // eslint-disable-next-line array-callback-return
+    Object.entries(priceData.data).find(([token, item]) => {
+      switch (token.toLowerCase()) {
+        case '0x1e94361329257647669dde7dc8c869624aa424ea':
+          cakePriceBusd = parseFloat(item.price);
+          break;
+      }
+    });
+  }
 
   const cakePriceBusdString =
     Number.isNaN(cakePriceBusd) || cakePriceBusd === 0 || !cakePriceBusd
