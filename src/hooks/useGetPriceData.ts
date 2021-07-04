62b8523ac569b2779b55dee06e312461867569f9
Peter Nahm <hwnahm@gmail.com>
2021-07-05 02:22:54 +0900

    fix: tal price on meta title

--- src/hooks/useGetPriceData.ts
+++ src/hooks/useGetPriceData.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 type ApiResponse = {
   updated_at: string
@@ -16,6 +16,8 @@ type ApiResponse = {
 const api = 'https://taalswap-api-ethereum.vercel.app/api/tokens';
 
 const useGetPriceData = () => {
+  // const talPrice = 0.0;
+  // const talPriceRef = useRef(talPrice);
   const [data, setData] = useState<ApiResponse | null>(null);
 
   useEffect(() => {
@@ -25,15 +27,26 @@ const useGetPriceData = () => {
         const res: ApiResponse = await response.json();
 
         setData(res);
+
+        // // eslint-disable-next-line array-callback-return
+        // const find = Object.entries(res.data).find(([token, item]) => {
+        //   switch (token.toLowerCase()) {
+        //     case '0x1e94361329257647669dde7dc8c869624aa424ea':
+        //       talPriceRef.current = parseFloat(item.price);
+        //       break;
+        //   }
+        // });
       } catch (error) {
         console.error('Unable to fetch price data:', error);
       }
     };
 
     fetchData();
+  // }, [setData, talPrice]);
   }, [setData]);
 
-  return data;
+  // return [data, talPrice];
+  return data
 };
 
 export default useGetPriceData;
