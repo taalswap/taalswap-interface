9d3003bd01c753a08ab3209fced337a7ac8fc6de
Peter Nahm <hwnahm@gmail.com>
2021-07-05 21:39:29 +0900

    chore: ropsten test again

--- src/utils/useGetCakeBusdLpPrice.tsx
+++ src/utils/useGetCakeBusdLpPrice.tsx
@@ -3,7 +3,7 @@ import { useTradeExactIn } from 'hooks/Trades'
 import { tryParseAmount } from 'state/swap/hooks'
 
 const useGetCakeBusdLpPrice = () => {
-  const talAddress = '0x1e94361329257647669dde7dc8c869624aa424ea'
+  const talAddress = '0x2ccdf53b17cce1c1c37bdd0ff0f8320e8cea34ed'
   const usdcAddress = '0x9c8fa1ee532f8afe9f2e27f06fd836f3c9572f71'
   const inputCurrency = useCurrency(talAddress)
   const outputCurrency = useCurrency(usdcAddress)
