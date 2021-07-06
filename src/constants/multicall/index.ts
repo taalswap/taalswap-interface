ab35b1dcb85f94a3b24e1d77b5dfcbf5afa17a1b
Peter Nahm <hwnahm@gmail.com>
2021-07-06 23:53:23 +0900

    chore: mainnet release

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -2,7 +2,7 @@ import { ChainId } from 'taalswap-sdk';
 import MULTICALL_ABI from './abi.json';
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb', // TODO
+  [ChainId.MAINNET]: '0xC72160167F700f842Ddf553e06Bc44186F42B929',
   [ChainId.ROPSTEN]: '0xE81BFF6533419DCA61cabCd26252EaFEc782aC00',
   [ChainId.RINKEBY]: '0x9B1265E5a12DED869CBe1934828aCe8388FA1362'
 };
