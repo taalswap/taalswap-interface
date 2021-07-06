f52a635cfcfb9db5e7bbeb77840c432e87c39d84
helloljho <helloljho@gmail.com>
2021-07-06 21:07:52 +0900

    add multi lang

--- src/components/Popups/TransactionPopup.tsx
+++ src/components/Popups/TransactionPopup.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { AlertCircle, CheckCircle } from 'react-feather';
 import { Text } from 'taalswap-uikit';
 import styled, { ThemeContext } from 'styled-components';
+import { useTranslation } from 'contexts/Localization';
 import { useActiveWeb3React } from '../../hooks';
 import { getBscScanLink } from '../../utils';
 import { ExternalLink } from '../Shared';
@@ -13,16 +14,16 @@ const RowNoFlex = styled(AutoRow)`
 `;
 
 export default function TransactionPopup({
-                                           hash,
-                                           success,
-                                           summary
-                                         }: {
-  hash: string
-  success?: boolean
-  summary?: string
+  hash,
+  success,
+  summary,
+}: {
+  hash: string;
+  success?: boolean;
+  summary?: string;
 }) {
   const { chainId } = useActiveWeb3React();
-
+  const { t } = useTranslation();
   const theme = useContext(ThemeContext);
 
   return (
@@ -34,9 +35,15 @@ export default function TransactionPopup({
           <AlertCircle color={theme.colors.failure} size={24} />
         )}
       </div>
-      <AutoColumn gap='8px'>
-        <Text>{summary ?? `Hash: ${hash.slice(0, 8)}...${hash.slice(58, 65)}`}</Text>
-        {chainId && <ExternalLink href={getBscScanLink(chainId, hash, 'transaction')}>View on EtherScan</ExternalLink>}
+      <AutoColumn gap="8px">
+        <Text>
+          {summary ?? `Hash: ${hash.slice(0, 8)}...${hash.slice(58, 65)}`}
+        </Text>
+        {chainId && (
+          <ExternalLink href={getBscScanLink(chainId, hash, 'transaction')}>
+            {t('View on EtherScan')}
+          </ExternalLink>
+        )}
       </AutoColumn>
     </RowNoFlex>
   );
