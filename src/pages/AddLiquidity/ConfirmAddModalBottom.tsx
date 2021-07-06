65e9b13776b78de5b20a6cf815028a849082028b
helloljho <helloljho@gmail.com>
2021-07-06 16:31:21 +0900

    add language

--- src/pages/AddLiquidity/ConfirmAddModalBottom.tsx
+++ src/pages/AddLiquidity/ConfirmAddModalBottom.tsx
@@ -1,67 +1,81 @@
-import { Currency, CurrencyAmount, Fraction, Percent } from 'taalswap-sdk'
-import React from 'react'
-import { Button, Text } from 'taalswap-uikit'
-import { RowBetween, RowFixed } from '../../components/Row'
-import CurrencyLogo from '../../components/CurrencyLogo'
-import { Field } from '../../state/mint/actions'
+import { Currency, CurrencyAmount, Fraction, Percent } from 'taalswap-sdk';
+import React from 'react';
+import { Button, Text } from 'taalswap-uikit';
+import { RowBetween, RowFixed } from '../../components/Row';
+import CurrencyLogo from '../../components/CurrencyLogo';
+import { Field } from '../../state/mint/actions';
 import { useTranslation } from '../../contexts/Localization';
 
 export function ConfirmAddModalBottom({
-                                        noLiquidity,
-                                        price,
-                                        currencies,
-                                        parsedAmounts,
-                                        poolTokenPercentage,
-                                        onAdd
-                                      }: {
-  noLiquidity?: boolean
-  price?: Fraction
-  currencies: { [field in Field]?: Currency }
-  parsedAmounts: { [field in Field]?: CurrencyAmount }
-  poolTokenPercentage?: Percent
-  onAdd: () => void
+  noLiquidity,
+  price,
+  currencies,
+  parsedAmounts,
+  poolTokenPercentage,
+  onAdd,
+}: {
+  noLiquidity?: boolean;
+  price?: Fraction;
+  currencies: { [field in Field]?: Currency };
+  parsedAmounts: { [field in Field]?: CurrencyAmount };
+  poolTokenPercentage?: Percent;
+  onAdd: () => void;
 }) {
   const { t } = useTranslation();
   return (
     <>
       <RowBetween>
-        <Text>{currencies[Field.CURRENCY_A]?.symbol} Deposited</Text>
+        {/* <Text>{currencies[Field.CURRENCY_A]?.symbol} Deposited</Text> */}
+        <Text>{`${currencies[Field.CURRENCY_A]?.symbol} ${t(
+          'Deposited'
+        )}`}</Text>
         <RowFixed>
-          <CurrencyLogo currency={currencies[Field.CURRENCY_A]} style={{ marginRight: '8px' }} />
+          <CurrencyLogo
+            currency={currencies[Field.CURRENCY_A]}
+            style={{ marginRight: '8px' }}
+          />
           <Text>{parsedAmounts[Field.CURRENCY_A]?.toSignificant(6)}</Text>
         </RowFixed>
       </RowBetween>
       <RowBetween>
-        <Text>{currencies[Field.CURRENCY_B]?.symbol} Deposited</Text>
+        {/* <Text>{currencies[Field.CURRENCY_B]?.symbol} Deposited</Text> */}
+        <Text>{`${currencies[Field.CURRENCY_B]?.symbol} ${t(
+          'Deposited'
+        )}`}</Text>
         <RowFixed>
-          <CurrencyLogo currency={currencies[Field.CURRENCY_B]} style={{ marginRight: '8px' }} />
+          <CurrencyLogo
+            currency={currencies[Field.CURRENCY_B]}
+            style={{ marginRight: '8px' }}
+          />
           <Text>{parsedAmounts[Field.CURRENCY_B]?.toSignificant(6)}</Text>
         </RowFixed>
       </RowBetween>
       <RowBetween>
         <Text>Rates</Text>
         <Text>
-          {`1 ${currencies[Field.CURRENCY_A]?.symbol} = ${price?.toSignificant(4)} ${
-            currencies[Field.CURRENCY_B]?.symbol
-          }`}
+          {`1 ${currencies[Field.CURRENCY_A]?.symbol} = ${price?.toSignificant(
+            4
+          )} ${currencies[Field.CURRENCY_B]?.symbol}`}
         </Text>
       </RowBetween>
       <RowBetween style={{ justifyContent: 'flex-end' }}>
         <Text>
-          {`1 ${currencies[Field.CURRENCY_B]?.symbol} = ${price?.invert().toSignificant(4)} ${
-            currencies[Field.CURRENCY_A]?.symbol
-          }`}
+          {`1 ${currencies[Field.CURRENCY_B]?.symbol} = ${price
+            ?.invert()
+            .toSignificant(4)} ${currencies[Field.CURRENCY_A]?.symbol}`}
         </Text>
       </RowBetween>
       <RowBetween>
         <Text>Share of Pool:</Text>
-        <Text>{noLiquidity ? '100' : poolTokenPercentage?.toSignificant(4)}%</Text>
+        <Text>
+          {noLiquidity ? '100' : poolTokenPercentage?.toSignificant(4)}%
+        </Text>
       </RowBetween>
-      <Button mt='20px' onClick={onAdd}>
+      <Button mt="20px" onClick={onAdd}>
         {noLiquidity ? t('Create Pool & Supply') : t('Confirm Supply')}
       </Button>
     </>
-  )
+  );
 }
 
-export default ConfirmAddModalBottom
+export default ConfirmAddModalBottom;
