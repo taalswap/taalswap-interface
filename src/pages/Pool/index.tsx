c4ba37672588fa929b8c9e69766d2e54e8bfbc27
helloljho <helloljho@gmail.com>
2021-07-06 16:44:45 +0900

    Pool not use Teaser

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -77,7 +77,7 @@ export default function Pool() {
 
   return (
     <Container>
-      <Teaser />
+      {/* <Teaser /> */}
       {/* <CardNav activeIndex={1} /> */}
       <AppBody>
         <PageHeader
