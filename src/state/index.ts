bc1cb773c1c6d1898c490546d4ee11cfff878cc9
helloljho <helloljho@gmail.com>
2021-07-12 13:36:24 +0900

    add devtool setting

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,33 +1,34 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
-import { save, load } from 'redux-localstorage-simple'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { save, load } from 'redux-localstorage-simple';
 
-import application from './application/reducer'
-import { updateVersion } from './global/actions'
-import user from './user/reducer'
-import transactions from './transactions/reducer'
-import swap from './swap/reducer'
-import mint from './mint/reducer'
-import lists from './lists/reducer'
-import burn from './burn/reducer'
-import multicall from './multicall/reducer'
-import toasts from './toasts'
-import { getThemeCache } from '../utils/theme'
+import application from './application/reducer';
+import { updateVersion } from './global/actions';
+import user from './user/reducer';
+import transactions from './transactions/reducer';
+import swap from './swap/reducer';
+import mint from './mint/reducer';
+import lists from './lists/reducer';
+import burn from './burn/reducer';
+import multicall from './multicall/reducer';
+import toasts from './toasts';
+import { getThemeCache } from '../utils/theme';
 
 type MergedState = {
   user: {
-    [key: string]: any
-  }
+    [key: string]: any;
+  };
   transactions: {
-    [key: string]: any
-  }
-}
-const PERSISTED_KEYS: string[] = ['user', 'transactions']
-const loadedState = load({ states: PERSISTED_KEYS }) as MergedState
+    [key: string]: any;
+  };
+};
+const PERSISTED_KEYS: string[] = ['user', 'transactions'];
+const loadedState = load({ states: PERSISTED_KEYS }) as MergedState;
 if (loadedState.user) {
-  loadedState.user.userDarkMode = getThemeCache()
+  loadedState.user.userDarkMode = getThemeCache();
 }
 
 const store = configureStore({
+  devTools: process.env.NODE_ENV !== 'production',
   reducer: {
     application,
     user,
@@ -37,15 +38,18 @@ const store = configureStore({
     burn,
     multicall,
     lists,
-    toasts
+    toasts,
   },
-  middleware: [...getDefaultMiddleware({ thunk: false }), save({ states: PERSISTED_KEYS })],
+  middleware: [
+    ...getDefaultMiddleware({ thunk: false }),
+    save({ states: PERSISTED_KEYS }),
+  ],
   preloadedState: loadedState,
-})
+});
 
-store.dispatch(updateVersion())
+store.dispatch(updateVersion());
 
-export default store
+export default store;
 
-export type AppState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type AppState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
