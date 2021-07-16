0e9922d42091be524a6054d1554b3055675329c6
Peter Nahm <hwnahm@gmail.com>
2021-07-16 19:03:39 +0900

    chore: enable info menu

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -58,8 +58,7 @@ const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
   {
     label: t('Info'),
     icon: 'InfoIcon',
-    // href: 'https://taalswap.info/home',
-    href: `${frontendBaseUrl}`,
+    href: 'https://taalswap.info/home',
   },
   {
     label: t('Partnership'),
