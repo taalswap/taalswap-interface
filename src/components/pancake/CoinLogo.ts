538375b40ac1132c454378477386788044a9d50a
helloljho <helloljho@gmail.com>
2021-07-19 17:03:25 +0900

    change coin list style

--- src/components/pancake/CoinLogo.ts
+++ src/components/pancake/CoinLogo.ts
@@ -1,9 +1,12 @@
-import styled from 'styled-components'
-import Logo from '../Logo'
+import styled from 'styled-components';
+import Logo from '../Logo';
 
 const CoinLogo = styled(Logo)<{ size: string }>`
   width: ${({ size }) => size};
   height: ${({ size }) => size};
-`
+  background-color: white;
+  border-radius: 50%;
+  box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
+`;
 
-export default CoinLogo
+export default CoinLogo;
