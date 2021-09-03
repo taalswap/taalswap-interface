04f714699726ec64e24b5ea270afd4e84e20807b
Peter Nahm <hwnahm@gmail.com>
2021-09-03 17:58:12 +0900

    fix : klaytn cypress multicall address

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -5,7 +5,7 @@ const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: '0xc72160167f700f842ddf553e06bc44186f42b929',
   [ChainId.ROPSTEN]: '0xe81bff6533419dca61cabcd26252eafec782ac00',
   [ChainId.RINKEBY]: '0xe81bff6533419dca61cabcd26252eafec782ac00',
-  [ChainId.KLAYTN]: '0x6943fa156da5a68c74696837666ae49c95712431',
+  [ChainId.KLAYTN]: '0x5ba315e48b40f8512b86e1795781f312d330c5fc',
   [ChainId.BAOBAB]: '0x6943fa156da5a68c74696837666ae49c95712431'
 };
 
