126f31df4ca80907eb9751f47b0de7d6b5d72d72
Peter Nahm <hwnahm@gmail.com>
2021-09-06 18:11:05 +0900

    fix: bug

--- src/utils/getChainId.ts
+++ src/utils/getChainId.ts
@@ -3,7 +3,7 @@ import { parseInt } from 'lodash'
 const getChainId = () => {
   const NETWORK_URL = process.env.REACT_APP_CHAIN_ID ?? '1';
   const chainIdStr = window.localStorage.getItem("chainId") ?? 'undefined'
-  const chainId = (!chainIdStr || chainIdStr === 'undefined' || chainIdStr === 'NaN')
+  const chainId = (!chainIdStr || chainIdStr === 'undefined' || chainIdStr === 'NaN' || chainIdStr === null)
     ? parseInt(NETWORK_URL, 10)
     : parseInt(chainIdStr, 10)
   return chainId
