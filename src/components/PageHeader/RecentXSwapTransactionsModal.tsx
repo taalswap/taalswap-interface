b67dda49af3998b75a11b851c5cc747755244749
helloljho <helloljho@gmail.com>
2021-11-22 13:24:57 +0900

    X-Swap transactions history

---
+++ src/components/PageHeader/RecentXSwapTransactionsModal.tsx
@@ -0,0 +1,329 @@
+import React, { useMemo, useState, useEffect, useCallback } from 'react';
+import {
+  Button,
+  CheckmarkCircleIcon,
+  ErrorIcon,
+  Flex,
+  Link,
+  LinkExternal,
+  Modal,
+  Text,
+} from 'taalswap-uikit';
+import { useActiveWeb3React } from 'hooks';
+import { useTheme } from 'styled-components';
+import { getBscScanLink } from 'utils';
+import {
+  isTransactionRecent,
+  useAllTransactions,
+} from 'state/transactions/hooks';
+import { TransactionDetails } from 'state/transactions/reducer';
+import Loader from 'components/Loader';
+import axios from 'axios';
+import { useTranslation } from '../../contexts/Localization';
+import TOKEN_LIST from '../../constants/token/taalswap.json';
+
+type RecentTransactionsModalProps = {
+  onDismiss?: () => void;
+};
+
+// TODO: Fix UI Kit typings
+const defaultOnDismiss = () => null;
+
+const newTransactionsFirst = (a: TransactionDetails, b: TransactionDetails) =>
+  b.addedTime - a.addedTime;
+
+const getRowStatus = (sortedRecentTransaction: TransactionDetails) => {
+  const { hash, receipt } = sortedRecentTransaction;
+
+  if (!hash) {
+    return { icon: <Loader />, color: 'text' };
+  }
+
+  if (hash && receipt?.status === 1) {
+    return { icon: <CheckmarkCircleIcon color="success" />, color: 'success' };
+  }
+
+  return { icon: <ErrorIcon color="failure" />, color: 'failure' };
+};
+
+const RecentXSwapTransactionsModal = ({
+  onDismiss = defaultOnDismiss,
+}: RecentTransactionsModalProps) => {
+  const { t } = useTranslation();
+  const { account, chainId } = useActiveWeb3React();
+  const allTransactions = useAllTransactions();
+  const [allXSwapTransactions, setAllXSwapTransactions] = useState([]);
+  const theme = useTheme();
+  const btnColor = theme.isDark ? '#fff' : '#212b36';
+
+  const ethDefaultUrl = 'https://ropsten.etherscan.io/tx';
+  const klaytnDefaultUrl = 'https://baobab.scope.klaytn.com/tx';
+
+  // Logic taken from Web3Status/index.tsx line 175
+  const sortedRecentTransactions = useMemo(() => {
+    const txs = Object.values(allTransactions);
+    return txs.filter(isTransactionRecent).sort(newTransactionsFirst);
+  }, [allTransactions]);
+
+  useEffect(() => {
+    const getXSWapTransactions = async () => {
+      //   if (account !== undefined) {
+      const url = `http://localhost:4000/bridge/api/user/${account}`;
+
+      await axios.get(url).then((response) => {
+        setAllXSwapTransactions(response.data.data);
+      });
+    };
+    // };
+
+    getXSWapTransactions();
+  }, [account]);
+
+  const getSymbolByAddress = useCallback(
+    (address: string | undefined, chain: string) => {
+      let curChainId;
+
+      if (chain === 'ETH') curChainId = 3;
+      else curChainId = 1001;
+
+      const curToken =
+        address !== undefined
+          ? TOKEN_LIST.tokens.find(
+              (token) =>
+                token.address === address.toLowerCase() &&
+                token.chainId === curChainId
+            )
+          : null;
+
+      return curToken?.symbol;
+    },
+    []
+  );
+
+  const getUrl = (txHash, fromChain) => {
+    let url;
+
+    switch (fromChain.toUpperCase()) {
+      case 'ETH':
+        url = `${ethDefaultUrl}/${txHash}`;
+        break;
+      case 'KLAYTN':
+        url = `${klaytnDefaultUrl}/${txHash}`;
+        break;
+    }
+
+    return url;
+  };
+
+  const getSummary = (
+    fromSymbol,
+    toSymbol,
+    formattedAmount,
+    formattedXAmount,
+    txHash,
+    xTxHash,
+    fromChain,
+    toChain
+  ) => {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-start',
+          //   flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <div style={{ marginRight: '0.3rem' }}>X-Swap</div>
+        <Link
+          style={{ marginRight: '0.3rem' }}
+          href={getUrl(txHash, fromChain)}
+          target="_blank"
+        >{`${formattedAmount
+          ?.toFixed(10)
+          .replace(/(0+$)/, '')} ${fromSymbol}`}</Link>
+        <div style={{ marginRight: '0.3rem' }}>for</div>
+        <Link
+          style={{ marginRight: '0.3rem' }}
+          href={getUrl(xTxHash, toChain)}
+          target="_blank"
+        >{`${formattedXAmount
+          ?.toFixed(10)
+          .replace(/(0+$)/, '')} ${toSymbol}`}</Link>
+      </div>
+    );
+  };
+
+  return (
+    <Modal
+      title={t('Recent X-Swap transactions')}
+      onDismiss={onDismiss}
+      style={{ position: 'relative' }}
+    >
+      {!account && (
+        <Flex
+          justifyContent="center"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <Text mb="8px" bold>
+            Please connect your wallet to view your recent transactions
+          </Text>
+          <div
+            style={{
+              position: 'absolute',
+              right: '20px',
+              top: '20px',
+              cursor: 'pointer',
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              onClick={onDismiss}
+            >
+              <g
+                id="___Icons_ic_replace"
+                data-name="__🥬Icons/ ic_replace"
+                transform="translate(0 16.971) rotate(-45)"
+              >
+                <g id="_gr" data-name="#gr">
+                  <path
+                    id="Path"
+                    d="M22.5,10.5h-9v-9a1.5,1.5,0,0,0-3,0v9h-9a1.5,1.5,0,0,0,0,3h9v9a1.5,1.5,0,0,0,3,0v-9h9a1.5,1.5,0,0,0,0-3Z"
+                    fill={btnColor}
+                  />
+                </g>
+              </g>
+            </svg>
+          </div>
+        </Flex>
+      )}
+      {/* {account && chainId && sortedRecentTransactions.length === 0 && ( */}
+      {account && chainId && allXSwapTransactions.length === 0 && (
+        <Flex
+          justifyContent="center"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <Text mb="8px" bold>
+            {t('No recent transactions')}
+          </Text>
+          <div
+            style={{
+              position: 'absolute',
+              right: '20px',
+              top: '20px',
+              cursor: 'pointer',
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              onClick={onDismiss}
+            >
+              <g
+                id="___Icons_ic_replace"
+                data-name="__🥬Icons/ ic_replace"
+                transform="translate(0 16.971) rotate(-45)"
+              >
+                <g id="_gr" data-name="#gr">
+                  <path
+                    id="Path"
+                    d="M22.5,10.5h-9v-9a1.5,1.5,0,0,0-3,0v9h-9a1.5,1.5,0,0,0,0,3h9v9a1.5,1.5,0,0,0,3,0v-9h9a1.5,1.5,0,0,0,0-3Z"
+                    fill={btnColor}
+                  />
+                </g>
+              </g>
+            </svg>
+          </div>
+        </Flex>
+      )}
+      {account &&
+        chainId &&
+        // sortedRecentTransactions.map((sortedRecentTransaction) => {
+        allXSwapTransactions.map((xswapTransaction) => {
+          const {
+            hash,
+            summary,
+            txHash,
+            xTxHash,
+            token,
+            xToken,
+            formattedAmount,
+            formattedXAmount,
+            fromChain,
+            toChain,
+          } = xswapTransaction;
+          const { icon, color } = getRowStatus(xswapTransaction);
+          const fromSymbol = getSymbolByAddress(token, fromChain);
+          const toSymbol = getSymbolByAddress(xToken, toChain);
+
+          return (
+            <>
+              <Flex
+                key={txHash}
+                alignItems="center"
+                justifyContent="space-between"
+                mb="4px"
+              >
+                {/* <LinkExternal
+                  href={getBscScanLink(chainId, hash, 'transaction')}
+                  color={color}
+                >
+                  {`X-Swap 1 ${fromSymbol} for 11 ${toSymbol}`}
+                </LinkExternal> */}
+                {getSummary(
+                  fromSymbol,
+                  toSymbol,
+                  formattedAmount,
+                  formattedXAmount,
+                  txHash,
+                  xTxHash,
+                  fromChain,
+                  toChain
+                )}
+                {/* {icon}
+                <div
+                  style={{
+                    position: 'absolute',
+                    right: '20px',
+                    top: '20px',
+                    cursor: 'pointer',
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 30 30"
+                    onClick={onDismiss}
+                  >
+                    <g
+                      id="___Icons_ic_replace"
+                      data-name="__🥬Icons/ ic_replace"
+                      transform="translate(0 16.971) rotate(-45)"
+                    >
+                      <g id="_gr" data-name="#gr">
+                        <path
+                          id="Path"
+                          d="M22.5,10.5h-9v-9a1.5,1.5,0,0,0-3,0v9h-9a1.5,1.5,0,0,0,0,3h9v9a1.5,1.5,0,0,0,3,0v-9h9a1.5,1.5,0,0,0,0-3Z"
+                          fill={btnColor}
+                        />
+                      </g>
+                    </g>
+                  </svg>
+                </div> */}
+              </Flex>
+            </>
+          );
+        })}
+    </Modal>
+  );
+};
+
+export default RecentXSwapTransactionsModal;
--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,62 +1,91 @@
-import React, { ReactNode } from 'react'
-import styled from 'styled-components'
-import { BackgroundImage, Flex, Heading, HistoryIcon, IconButton, Text, TuneIcon, useModal } from 'taalswap-uikit'
-import SettingsModal from './SettingsModal'
-import RecentTransactionsModal from './RecentTransactionsModal'
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+import {
+  BackgroundImage,
+  Flex,
+  Heading,
+  HistoryIcon,
+  IconButton,
+  Text,
+  TuneIcon,
+  useModal,
+} from 'taalswap-uikit';
+import SettingsModal from './SettingsModal';
+import RecentTransactionsModal from './RecentTransactionsModal';
 import { useTranslation } from '../../contexts/Localization';
-import OptionIcon from "../../pages/Swap/images/option_icon.svg";
-import Disclosure from "../../pages/Swap/images/disclosure.svg"
-
+import OptionIcon from '../../pages/Swap/images/option_icon.svg';
+import Disclosure from '../../pages/Swap/images/disclosure.svg';
+import Bridge from '../../pages/XSwap/images/bridge.svg';
+import RecentXSwapTransactionsModal from './RecentXSwapTransactionsModal';
 
 interface PageHeaderProps {
-  title: ReactNode
-  description?: ReactNode
-  children?: ReactNode
+  title: ReactNode;
+  description?: ReactNode;
+  children?: ReactNode;
 }
 /* to do */
 const StyledPageHeader = styled.div`
   border-bottom: 0px solid ${({ theme }) => theme.colors.disabled};
   padding: 0px 0px 24px 0px;
   width: 100%;
-  max-width:1070px;
+  max-width: 1070px;
 
   @media screen and (max-width: 500px) {
     padding-bottom: 0.6875rem;
   }
-`
+`;
 
 const Details = styled.div`
   flex: 1;
-`
+`;
 
 const PageHeader = ({ title, description, children }: PageHeaderProps) => {
   const { t } = useTranslation();
-  const [onPresentSettings] = useModal(<SettingsModal />)
-  const [onPresentRecentTransactions] = useModal(<RecentTransactionsModal />)
+  const [onPresentSettings] = useModal(<SettingsModal />);
+  const [onPresentRecentTransactions] = useModal(<RecentTransactionsModal />);
+  const [onPresentRecentXSwapTransactions] = useModal(
+    <RecentXSwapTransactionsModal />
+  );
 
   return (
     <StyledPageHeader>
-      <Flex alignItems='center'>
+      <Flex alignItems="center">
         <Details>
-          <Heading mb='8px'>{title}</Heading>
+          <Heading mb="8px">{title}</Heading>
           {description && (
-            <Text color='#a7b2b3' fontSize='14px'>
+            <Text color="#a7b2b3" fontSize="14px">
               {description}
             </Text>
           )}
         </Details>
-        <IconButton variant='text' onClick={onPresentSettings} title={t('Settings')}>
-          {/* <TuneIcon width='24px' color='#00ab55' /> */}
-          <img src={ OptionIcon } alt="option_icon" className="" />
-        </IconButton>
-        <IconButton variant='text' onClick={onPresentRecentTransactions} title={t('Recent transactions')}>
+        <IconButton
+          variant="text"
+          onClick={onPresentRecentXSwapTransactions}
+          title={t('Recent X-Swap transactions')}
+        >
           {/* <HistoryIcon width='24px' color='#00ab55' /> */}
-          <img src={ Disclosure } alt="option_icon" className="" />
+          <img src={Bridge} alt="option_icon" className="" />
+        </IconButton>
+        <IconButton
+          variant="text"
+          onClick={onPresentSettings}
+          title={t('Settings')}
+        >
+          {/* <TuneIcon width='24px' color='#00ab55' /> */}
+          <img src={OptionIcon} alt="option_icon" className="" />
+        </IconButton>
+        <IconButton
+          variant="text"
+          onClick={onPresentRecentTransactions}
+          title={t('Recent transactions')}
+        >
+          {/* <HistoryIcon width='24px' color='#00ab55' /> */}
+          <img src={Disclosure} alt="option_icon" className="" />
         </IconButton>
       </Flex>
-      {children && <Text mt='16px'>{children}</Text>}
+      {children && <Text mt="16px">{children}</Text>}
     </StyledPageHeader>
-  )
-}
+  );
+};
 
-export default PageHeader
+export default PageHeader;
